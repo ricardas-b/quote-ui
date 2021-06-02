69d383c2791cd6d61a7bbe1d9df5820e0a61c571
Ricardas B <ricardas.brazinskas@gmail.com>
2021-06-02 16:33:05 +0200

    Improve the visual design of <Label> component

--- src/Label.js
+++ src/Label.js
@@ -3,13 +3,15 @@ import './Label.css';
 
 
 class Label extends React.Component {
+    /* Used to represent content tags */
+
     constructor(props) {
         super(props);
     }
 
     render() {
         return (
-            <span className="label label-info">{this.props.tagText}</span>
+            <span className="label label-info"> {this.props.tagText} </span>
         );
     }
 }
